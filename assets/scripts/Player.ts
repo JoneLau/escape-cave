31b97264934f0d19e00d2bf1b24df34dd88c09dd
Sijie <devhacker520@hotmail.com>
2023-05-14 09:01:51 +0800

    新增了一个简单的状态控制

--- assets/scripts/Player.ts
+++ assets/scripts/Player.ts
@@ -1,10 +1,59 @@
-import { _decorator, Component, Node, Vec3 } from 'cc';
+import { _decorator, Component, Node, Vec3, Animation, AnimationState } from 'cc';
 const { ccclass, property } = _decorator;
+
+enum State {
+    IDLE = 0,
+    RUN = 1,
+    JUMP = 2,
+}
 
 @ccclass('Player')
 export class Player extends Component {
 
     _moveDelta: number = 0;
+
+    _jumping: boolean = false;
+    _state: State = State.IDLE;
+    setState(state: State) {
+        if (this._jumping) {
+            return;
+        }
+        if (this._state === state) {
+            return;
+        }
+        switch (state) {
+            case State.RUN: {
+                const animComp = this.node.getComponent(Animation) as Animation;
+                animComp.play('run');
+                break;
+            }
+            case State.IDLE: {
+                const animComp = this.node.getComponent(Animation) as Animation;
+                animComp.play('idle');
+                break;
+            }
+            case State.JUMP: {
+                this._jumping = true;
+                const animComp = this.node.getComponent(Animation) as Animation;
+                animComp.play('jumpFromGround');
+                debugger;
+                // 触碰地板后停止，要加函数钩子，可能要物理系统之类的配合
+                setTimeout(() => {
+                    debugger
+                    this._jumping = false;
+                    this.setState(State.IDLE);                    
+                }, 1000);
+                // animComp.once('finished', () => {
+                //     this._jumping = false;
+                //     this.setState(State.IDLE);
+                // });
+                break;
+            }
+        }
+        this._state = state;
+        
+    }
+
     start() {
 
     }
@@ -20,20 +69,20 @@ export class Player extends Component {
     moveLeft(delta: number) {
         this._moveDelta = -delta / 100;
         this.node.scale = new Vec3(-1, 1, 1);
+        this.setState(State.RUN);
     }
 
     moveRight(delta: number) {
         this._moveDelta = delta / 100;
         this.node.scale = new Vec3(1, 1, 1);
+        this.setState(State.RUN);
     }
 
-    _jumping: boolean = false;
     moveUp(delta: number) {
         if (delta < 40 || this._jumping) {
             return;
         }
-
-        // animComp.play('xx');
+        this.setState(State.JUMP);
     }
 
     moveDown(delta: number) {
@@ -42,6 +91,7 @@ export class Player extends Component {
 
     stop() {
         this._moveDelta = 0;
+        this.setState(State.IDLE);
     }
 }
 
